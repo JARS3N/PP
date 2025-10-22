9206720b75f0665dfa8dbcdf814c497787f84a5e
JARS3N <JARS3N@users.noreply.github.com>
2025-10-22 14:25:15 -0400

    Remove 'percision' parameter from dissolved_O2_wag function

--- R/dissolved_O2_wag.R
+++ R/dissolved_O2_wag.R
@@ -1,5 +1,5 @@
-dissolved_O2_wag<-function(TC,ap=atm,percision){
+dissolved_O2_wag<-function(TC,ap=atm){
   if(TC>=0 & TC<30){coef<-.678;adj<-35}else{if(TC>=30 & TC<=50){coef<-.827;adj<-49}}
-  vp<-wagner_vapor(TC,percision)
+  vp<-wagner_vapor(TC)
   ((ap-vp)*coef)/(adj+TC)
 }
