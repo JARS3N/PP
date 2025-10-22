59111db713721d328ab31017e107beb0d12d26d9
JARS3N <JARS3N@users.noreply.github.com>
2025-10-22 14:16:45 -0400

    Update ppO2.R

--- R/ppO2.R
+++ R/ppO2.R
@@ -3,3 +3,8 @@ O2<-Vectorize(function(TC,atm=760,percision='low'){
   mole_frac(TC,atm,percision)*760
   })
 
+
+O2_wag<-Vectorize(function(TC,atm=760,percision='low'){
+  #atm isn't used, the 760 is just unit conversion
+  mole_frac_wag(TC,atm,percision)*760
+  })
