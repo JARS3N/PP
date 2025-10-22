6267b79dc76e8ad9aa92b4ecf91ae945b5d97dea
JARS3N <JARS3N@users.noreply.github.com>
2025-10-22 14:27:30 -0400

    Refactor O2_wag function to remove precision parameter

--- R/ppO2.R
+++ R/ppO2.R
@@ -4,7 +4,8 @@ O2<-Vectorize(function(TC,atm=760,percision='low'){
   })
 
 
-O2_wag<-Vectorize(function(TC,atm=760,percision='low'){
+O2_wag<-Vectorize(function(TC,atm=760){
   #atm isn't used, the 760 is just unit conversion
-  mole_frac_wag(TC,atm,percision)*760
+  mole_frac_wag(TC,atm)*760
   })
+
