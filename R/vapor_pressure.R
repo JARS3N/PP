9a42849b04b40a8bc32f34c221394e1c316524ce
JARS3N <JARS3N@users.noreply.github.com>
2025-10-29 14:13:10 -0400

    Refactor vapor_pressure function for simplicity

--- R/vapor_pressure.R
+++ R/vapor_pressure.R
@@ -1,6 +1,7 @@
 vapor_pressure <- function(Temp, percision = 'low') {
-  X = list(high = c(0.0455712421903711,-0.855936034546114,16.5093235942667),
-  low = c(0.0456,- 0.8559,16.509))[[percision]]
+ # X = list(high = c(0.0455712421903711,-0.855936034546114,16.5093235942667),
+  #low = c(0.0456,- 0.8559,16.509))[[percision]]
+  X <- c(0.0456,- 0.8559,16.509)
   (X[1]*Temp^2)+(X[2]*Temp)+X[3]
 }
 
@@ -17,3 +18,4 @@ weiss <- function(TC,ap) {
  # apply correction factor for localized atmospheric pressure
  C*((ap-pp::wagner_vapor(TC))/(760-pp::wagner_vapor(TC)))
 }
+
