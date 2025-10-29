a1fdf48a7b9c6640afdd157534ba6fa17b51357a
JARS3N <JARS3N@users.noreply.github.com>
2025-10-29 12:50:29 -0400

    Refactor dissolved_O2 function for clarity

--- R/DO.R
+++ R/DO.R
@@ -1,5 +1,6 @@
-dissolved_O2<-function(TC,ap=atm,percision){
+dissolved_O2<-function(TC,ap=atm){
   if(TC>=0 & TC<30){coef<-.678;adj<-35}else{if(TC>=30 & TC<=50){coef<-.827;adj<-49}}
-  vp<-vapor_pressure(TC,percision)
+  vp<-vapor_pressure(TC)
   ((ap-vp)*coef)/(adj+TC)
 }
+
