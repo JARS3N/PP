b8788baa2951162e8d012c6dfd0319d415ed4dca
JARS3N <JARS3N@users.noreply.github.com>
2025-11-13 14:46:29 -0500

    Refactor mole_frac function to simplify parameters

--- R/mole_frac.R
+++ R/mole_frac.R
@@ -3,7 +3,8 @@ mole_frac <- function(TC, atm,percision) {
   mw_H20 <- 18
   mw_O2 <- 32
   DO <-
-    dissolved_O2(TC, atm, percision) * (mw_H20 / wt_H20 / 1000 / mw_O2)
+    dissolved_O2(TC, atm) * (mw_H20 / wt_H20 / 1000 / mw_O2)
 
   DO * henry_constant(TC, percision)
 }
+
