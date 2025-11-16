5758b760158581136b3df18ba08882ad1a493159
JARS3N <JARS3N@users.noreply.github.com>
2025-11-16 16:02:17 -0500

    Refactor dissolved oxygen functions and add documentation

--- R/DO.R
+++ R/DO.R
@@ -1,33 +1,45 @@
-dissolved_O2<-function(TC,ap=atm){
-  if(TC>=0 & TC<30){coef<-.678;adj<-35}else{if(TC>=30 & TC<=50){coef<-.827;adj<-49}}
-  vp<-vapor_pressure(TC)
-  ((ap-vp)*coef)/(adj+TC)
-}
-
-
-DO_split<-function (TC, ap,vp ) {
-  if (TC >= 0 & TC < 30) {
+# ==============================================================================
+# R/dissolved_oxygen.R
+# ==============================================================================
+#' Calculate Dissolved Oxygen - Piecewise Approximation
+#' 
+#' @param tc Temperature in Celsius
+#' @param ap Atmospheric pressure in mmHg
+#' @param vp Vapor pressure in mmHg
+#' @return Dissolved oxygen in mg/L
+#' @export
+DO_approx <- function(tc, ap, vp) {
+  if (tc >= 0 && tc < 30) {
     coef <- 0.678
     adj <- 35
-  }else {
-    if (TC >= 30 & TC <= 50) {
-      coef <- 0.827
-      adj <- 49
-    }
+  } else if (tc >= 30 && tc <= 50) {
+    coef <- 0.827
+    adj <- 49
+  } else {
+    stop("Temperature out of range [0, 50] for approx method")
   }
-  ((ap - vp) * coef)/(adj + TC)
+  ((ap - vp) * coef) / (adj + tc)
 }
 
-DO_weiss <- function(TC,ap,vp) {
-  #dissolved oxygen in mg/L
-  A1 = -139.34411
-  A2 = 1.575701e5
-  A3 = -6.642308e7
-  A4 = 1.243800e10
-  A5 = -8.621949e11
-  T <- TC + 273.15
-  lnC <- A1 + (A2 / T) + (A3 / T^2) + (A4 / T^3) + (A5 / T^4)
- C<- exp(lnC)  #mg/L
- # apply correction factor for localized atmospheric pressure
- C*((ap-vp)/(760-vp))
+#' Calculate Dissolved Oxygen - Weiss Equation
+#' 
+#' @param tc Temperature in Celsius
+#' @param ap Atmospheric pressure in mmHg
+#' @param vp Vapor pressure in mmHg
+#' @return Dissolved oxygen in mg/L
+#' @export
+DO_weiss <- function(tc, ap, vp) {
+  A1 <- -139.34411
+  A2 <- 157570.1
+  A3 <- -66423080.0
+  A4 <- 1.2438e10
+  A5 <- -8.621949e11
+  
+  T <- tc + 273.15
+  invT <- 1.0 / T
+  
+  lnC <- A1 + A2*invT + A3*(invT^2.0) + A4*(invT^3.0) + A5*(invT^4.0)
+  C <- exp(lnC)
+  C * ((ap - vp) / (760.0 - vp))
 }
+
