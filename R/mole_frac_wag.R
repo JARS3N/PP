86eb0e95dd5ff995ec31892fad985091db0fa5d9
JARS3N <JARS3N@users.noreply.github.com>
2025-11-16 16:08:32 -0500

    Delete R/mole_frac_wag.R

--- R/mole_frac_wag.R
+++
@@ -1,9 +0,0 @@
-mole_frac_wag <- function(TC, atm) {
-  wt_H20 <- 1000
-  mw_H20 <- 18
-  mw_O2 <- 32
-  DO <-
-    dissolved_O2_wag(TC, atm) * (mw_H20 / wt_H20 / 1000 / mw_O2)
-
-  DO * henry_constant(TC)
-}
