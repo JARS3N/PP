08b0c6b3acb2daacca3a5a12c8ae373806473ef5
JARS3N <JARS3N@users.noreply.github.com>
2025-11-16 16:19:13 -0500

    Vectorize O2 function for better input handling

--- R/O2.R
+++ R/O2.R
@@ -1,26 +1,39 @@
-O2 <- function(tc, ap, method = "legacy") {
-  method <- match.arg(method, c("legacy", "ideal"))
-  
-  # Constants
-  MW_O2 <- 32.0
-  Wt_H2O <- 1000.0
-  MW_H2O <- 18.0
-  TorrPerAtm <- 760.0
-  
-  # Calculate based on method
-  henry <- HC(tc)  # units: atm/mol
-  
-  if (method == "legacy") {
-    vp <- vp_poly(tc)
-    do_mgL <- DO_approx(tc, ap, vp)
-  } else {  # ideal
-    vp <- vp_wagner(tc)
-    do_mgL <- DO_weiss(tc, ap, vp)
-  }
-  
-  # Final calculation (same for both methods)
-  do_molL <- (do_mgL / 1000.0) / MW_O2     # units: mol/L
-  mol_frac <- do_molL / (Wt_H2O / MW_H2O)  # units: moles
-  
-  mol_frac * henry * TorrPerAtm  # units: mmHg
-}
+#' Calculate Partial Pressure of Oxygen
+#' 
+#' @param tc Temperature in Celsius (well temperature), can be a vector
+#' @param ap Atmospheric pressure in mmHg, can be a vector
+#' @param method Character string: "legacy" (default) or "ideal"
+#' @return Partial pressure of O2 in mmHg (Torr)
+#' @export
+#' @examples
+#' O2(37, 760)                  # Uses legacy method (vp_poly + DO_approx)
+#' O2(37, 760, method="ideal")  # Uses ideal method (vp_wagner + DO_weiss)
+#' O2(c(25, 30, 37), 760)       # Vectorized
+O2 <- Vectorize(
+  function(tc, ap, method = "legacy") {
+    method <- match.arg(method, c("legacy", "ideal"))
+    
+    # Constants
+    MW_O2 <- 32.0
+    Wt_H2O <- 1000.0
+    MW_H2O <- 18.0
+    TorrPerAtm <- 760.0
+    
+    # Calculate based on method
+    henry <- HC(tc)
+    
+    if (method == "legacy") {
+      vp <- vp_poly(tc)
+      do_mgL <- DO_approx(tc, ap, vp)
+    } else {
+      vp <- vp_wagner(tc)
+      do_mgL <- DO_weiss(tc, ap, vp)
+    }
+    
+    do_molL <- (do_mgL / 1000.0) / MW_O2
+    mol_frac <- do_molL / (Wt_H2O / MW_H2O)
+    
+    mol_frac * henry * TorrPerAtm
+  },
+  vectorize.args = c("tc")
+)
